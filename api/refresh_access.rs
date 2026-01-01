8eb38390bb2339f2c9e231f5aa513cc1959051c4
Souchy <robyn.g@hotmail.fr>
2025-12-31 19:48:21 -0500

    WIP routes

---
+++ api/refresh_access.rs
@@ -0,0 +1,17 @@
+use Rausth::config::{AuthProviderType, SETTINGS};
+use serde_json::json;
+use vercel_runtime::{Body, Error, Request, Response, StatusCode, run};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
+
+pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(Body::from(serde_json::to_vec(
+            &json!({ "message": "Hello world" }),
+        )?))?)
+}
--- api/signin.rs
+++ api/signin.rs
@@ -1,5 +1,4 @@
 use Rausth::config::{AuthProviderType, SETTINGS};
-use serde_json::json;
 use vercel_runtime::{Body, Error, Request, Response, StatusCode, run};
 
 #[allow(dead_code)]
@@ -8,9 +7,12 @@ async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
+/**
+ * /api/signin/{provider}
+ * e.g. /api/signin/microsoft
+ * Handles the sign-in request by redirecting to the appropriate authentication provider's login URL.
+ */
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
-    // let body = serde_json::to_vec(&json!({ "message": "Hello world" }))?;
-
     let path = _req.uri().path();
 
     // normalize and extract the provider segment from paths like "/api/microsoft" or "/api/login/microsoft"
@@ -23,28 +25,30 @@ pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
         .ok_or_else(|| Error::from("Provider not specified in the URL"))?;
 
     // Get AuthProviderType from provider_str
-    let auth_provider: AuthProviderType = AuthProviderType::from(provider_segment);
-    if auth_provider == AuthProviderType::Unknown(provider_segment.to_string()) {
+    let provider_type: AuthProviderType = AuthProviderType::from(provider_segment);
+    if provider_type == AuthProviderType::Unknown(provider_segment.to_string()) {
         return Err(Error::from(format!("Unknown authentication provider: {}", provider_segment)));
     }
     
-    let provider_sttings = SETTINGS.auth_providers.iter().find(|p| p.name == auth_provider && p.active).ok_or_else(|| {
-        Error::from(format!("Authentication provider '{}' not found or inactive", provider_segment))
-    })?;
+    let provider_settings = SETTINGS.auth_providers.iter()
+        .find(|p| p.name == provider_type && p.enabled).ok_or_else(|| {
+            Error::from(format!("Authentication provider '{}' not found or inactive", provider_segment))
+        })?;
 
     let pkce_challenge = "example_challenge"; // Replace with actual PKCE challenge
     let login_url = format!(
         "{}?client_id={}&response_type=code&redirect_uri={}&scope={}&code_challenge={}&code_challenge_method=S256",
-        provider_sttings.auth_url,
-        provider_sttings.client_id,
-        provider_sttings.redirect_uri,
-        provider_sttings.scopes,
+        provider_settings.auth_url,
+        provider_settings.client_id,
+        provider_settings.redirect_uri,
+        provider_settings.scopes,
         pkce_challenge
     );
-    let body = serde_json::to_vec(&json!({ "login_url": login_url }))?;
 
+    // return as a redirect response
     Ok(Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "application/json")
-        .body(Body::from(body))?)
+        .status(StatusCode::FOUND)
+        .header("Location", login_url)
+        .body(Body::Empty)?
+    )
 }
